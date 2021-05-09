f836ad48ea74d22a8c425710a31c5533f54b9be3
Henry-Cantrell <hencan.1997@gmail.com>
2021-05-09 18:06:47 -0400

    flights controller added/routes update/flight view update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airport.create(airport_code:'SFO')
+Airport.create(airport_code:'NYC')
+
+Flight.create(origin_id:1, destination_id:2, duration:5, departure_date:2.months.ago)
+Flight.create(origin_id:2, destination_id:1, duration:6, departure_date:3.months.ago)
+Flight.create(origin_id:1, destination_id:2, duration:10, departure_date:2.months.ago)
+Flight.create(origin_id:2, destination_id:1, duration:7, departure_date:3.months.ago)
