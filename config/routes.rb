341785f03d0bdb09e30c2d62588b02b890816416
Henry-Cantrell <hencan.1997@gmail.com>
2021-07-01 12:53:46 -0400

    Updated flight search form to form helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "flights#index"
 
-  resources :flights
+  resources :flights do
+	collection do
+		post :lookup
+	end
+  end
+
   resources :bookings
+
 end
